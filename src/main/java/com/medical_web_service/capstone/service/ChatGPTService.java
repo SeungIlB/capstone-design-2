0f1b5057a7a1180da2425ef6dad66be60c7d90d7
승일 <seoung1515@naver.com>
2024-10-05 01:53:22 +0900

    ã지피티 기능 추가

---
+++ src/main/java/com/medical_web_service/capstone/service/ChatGPTService.java
@@ -0,0 +1,29 @@
+package com.medical_web_service.capstone.service;
+
+
+import com.medical_web_service.capstone.dto.ChatCompletionDto;
+import com.medical_web_service.capstone.dto.CompletionDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ChatGPT 서비스 인터페이스
+ *
+ * @author : lee
+ * @fileName : ChatGPTService
+ * @since : 12/29/23
+ */
+
+@Service
+public interface ChatGPTService {
+
+    List<Map<String, Object>> modelList();
+
+    Map<String, Object> isValidModel(String modelName);
+
+    Map<String, Object> legacyPrompt(CompletionDto completionDto);
+
+    Map<String, Object> prompt(ChatCompletionDto chatCompletionDto);
+}
